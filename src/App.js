2ddcd3b2d708b3061c93af389474cab590b58758
johnzama <johnsparta2121@gmail.com>
2024-10-15 14:40:46 -0400

    Set up components and routing for Cameroon History project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import History from './components/History';
+import Culture from './components/Culture';
+import Geography from './components/Geography';
+import Food from './components/Food';
+import ArtCraft from './components/ArtCraft';
+import ModernCameroon from './components/ModernCameroon';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Cameroon History and Culture</h1>
+          <nav>
+            <ul>
+              <li><Link to="/history">History</Link></li>
+              <li><Link to="/culture">Culture</Link></li>
+              <li><Link to="/geography">Geography</Link></li>
+              <li><Link to="/food">Food</Link></li>
+              <li><Link to="/art-craft">Art & Craft</Link></li>
+              <li><Link to="/modern">Modern Cameroon</Link></li>
+            </ul>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path="/history" element={<History />} />
+          <Route path="/culture" element={<Culture />} />
+          <Route path="/geography" element={<Geography />} />
+          <Route path="/food" element={<Food />} />
+          <Route path="/art-craft" element={<ArtCraft />} />
+          <Route path="/modern" element={<ModernCameroon />} />
+          <Route path="/" element={<History />} /> {/* Default route */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
